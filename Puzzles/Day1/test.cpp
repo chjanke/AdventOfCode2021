95804cd5eee7fbb0f4cf34b237eec0c87b241759
Chris <Chrishierso@googlemail.com>
2021-12-21 22:56:44 +0100

    add example test

--- Puzzles/Day1/test.cpp
+++ Puzzles/Day1/test.cpp
@@ -1,6 +1,27 @@
-#include "boost/ut.hpp"
+#include <boost/ut.hpp>
+
+#include "day1.hpp"
 
 int main()
 {
+	using namespace boost::ut;
+	std::string example =R"(199
+200
+208
+210
+200
+207
+240
+269
+260
+263)";
 
+	"Day1::star_one"_test = [&] {
+		std::istringstream in{ example };
+		expect(AoC::Day1::star_one(in) == 7_i);
+	};
+	"Day1::star_two"_test = [&] {
+		std::istringstream in{ example };
+		expect(AoC::Day1::star_two(in) == 5_i);
+	};
 }
